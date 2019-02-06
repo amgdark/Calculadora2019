801f5f9e4855517933b00b1c57e85eaeda84f5e4
Alejandro Mauricio <alexmau@MacBook-Pro-de-Alejandro-2.local>
2019-02-06 09:04:58 -0600

    Agrega prueba de -5 mas 10

--- TestCalculadora.py
+++ TestCalculadora.py
@@ -13,6 +13,10 @@ class TestCalculadora(unittest.TestCase):
 		calc = Calculadora()
 		self.assertEquals(15, calc.suma(5,10))
 
+	def test_suma_menos5_mas_10(self):
+		calc = Calculadora()
+		self.assertEquals(5, calc.suma(-5,10))
+
 
 
 if __name__ == '__main__':
