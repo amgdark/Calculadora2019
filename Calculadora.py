740fae97deedf6925fcac0eccf185b9aa8e5eafc
Alejandro Mauricio <alexmau@MacBook-Pro-de-Alejandro-2.local>
2019-02-25 08:53:15 -0600

    Corrige estándar de codigo

--- Calculadora.py
+++ Calculadora.py
@@ -1,7 +1,6 @@
 
 
 class Calculadora(object):
-	
 
-	def suma(self, num1, num2):
-		return num1 + num2
+    def suma(self, num1, num2):
+        return num1 + num2
--- TestCalculadora.py
+++ TestCalculadora.py
@@ -2,24 +2,21 @@ import unittest
 
 from Calculadora import Calculadora
 
+
 class TestCalculadora(unittest.TestCase):
 
+    def test_suma_2_mas_2(self):
+        calc = Calculadora()
+        self.assertEquals(4, calc.suma(2, 2))
 
-	def test_suma_2_mas_2(self):
-		calc = Calculadora()
-		self.assertEquals(4, calc.suma(2,2))
+    def test_suma_5_mas_10(self):
+        calc = Calculadora()
+        self.assertEquals(15, calc.suma(5, 10))
 
-	def test_suma_5_mas_10(self):
-		calc = Calculadora()
-		self.assertEquals(15, calc.suma(5,10))
-
-	def test_suma_menos5_mas_10(self):
-		calc = Calculadora()
-		self.assertEquals(5, calc.suma(-5,10))
-
+    def test_suma_menos5_mas_10(self):
+        calc = Calculadora()
+        self.assertEquals(5, calc.suma(-5, 10))
 
 
 if __name__ == '__main__':
-	unittest.main()
-
-
+    unittest.main()
